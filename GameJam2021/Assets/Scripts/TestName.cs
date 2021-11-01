20d84998e6130be3eebca15ff142dfd52c0edc9d
MathiasSorinPro <ld.mathias.sorin@gmail.com>
2021-11-01 16:45:35 -0400

    Added name script + coconut

---
+++ GameJam2021/Assets/Scripts/TestName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//WRITE YOUR NAME BELOW
+//MATHIAS
+
+public class TestName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
