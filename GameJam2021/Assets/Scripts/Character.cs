5580cdde9c877cd106c644d7dfefbe7e88fa45a8
MathiasSorinPro <ld.mathias.sorin@gmail.com>
2021-11-01 18:49:22 -0400

    added character script

---
+++ GameJam2021/Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Salut les ptits potes
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
