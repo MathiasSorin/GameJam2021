3a7630ed6cf27ffbb0faa4a07d798af56c2e5376
PhilippeSylvestre <ld.philippe.sylvestre@gmail.com>
2021-11-12 20:05:43 -0500

    Phil
    Jtesuce nyros 1

---
+++ GameJam2021/Assets/Scripts/JteSuceNyros.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JteSuceNyros : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 8===D
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
